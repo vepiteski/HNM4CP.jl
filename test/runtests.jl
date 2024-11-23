a8da42239c17bb440dcc8f43d1978dcdfa8cf527
Jean-Pierre Dussault <vepiteski@gmail.com>
2024-11-22 20:42:16 -0500

    Add test Fathi

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,10 @@ using Test
 
 @testset "HNM4CP.jl" begin
     # Write your tests here.
+    println("Test on lcprand")
+    println("------------------")
     include("nm_Test_lcprand.jl")
+    println("Test on fathi")
+    println("------------------")
+    include(nm_Test_Fathi.jl")
 end

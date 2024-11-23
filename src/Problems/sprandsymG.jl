82a6440e864e026f2280c1fc7cfafb026aed964f
Jean-Pierre Dussault <vepiteski@gmail.com>
2024-11-22 19:15:41 -0500

    sprandsym now accepts abstract floats

--- src/Problems/sprandsymG.jl
+++ src/Problems/sprandsymG.jl
@@ -1,4 +1,4 @@
-function sprandsym(n,dens,eigv; rng = Xoshiro() )
+function sprandsym(n, dens, eigv; T = Float64, rng = Xoshiro() )
     # ignore dens rounded to 1/2 or 1 for the moment
     #v = mu:(L-mu)/(n-1):L
     
@@ -7,10 +7,10 @@ function sprandsym(n,dens,eigv; rng = Xoshiro() )
     szblk = Int(round(n*dens,RoundUp))
     nbblk = Int(round(n/szblk,RoundUp))
 
-    Q = spzeros(n,n)
+    Q = spzeros(T, n,n)
     for blk = 0:(nbblk-1)
         In = 1+szblk*blk : min(n,(blk+1)*szblk)
-        Mi = rand(rng, length(In),length(In))
+        Mi = rand(rng, T, length(In),length(In))
         Oi, bidon = qr(Mi) # espérons que O est aléatoire orthogonale
         Qi = Oi'*Matrix(Diagonal(eigv[In]))*Oi
         Q[In,In] = Qi
@@ -22,15 +22,15 @@ function sprandsym(n,dens,eigv; rng = Xoshiro() )
 end
 
 using Test
-function testsprandsym()
-    n=100
+function testsprandsym(;T=Float64)
+    n=50
     dens=0.3
-    eigv=Array(1.0:100.0)
+    eigv=Array{T}(1.0:50.0)
 
-    Q = sprandsym(n,dens,eigv)
+    Q = sprandsym(n,dens,eigv, T = T)
 
     @test isapprox(nnz(Q)/(n^2), dens, rtol = 0.1)
-    @test cond(Array(Q)) ≈ 100.0
+    @test cond(Array(Q)) ≈ 50.0
 
     spy(Q)
 end
